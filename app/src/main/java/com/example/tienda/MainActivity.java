080224320bff0bca27b44c4c62952585294a19cc
ChristianVillamizar <113570709+ChristianVillamizar@users.noreply.github.com>
2022-11-18 10:41:44 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/tienda/MainActivity.java
+++ app/src/main/java/com/example/tienda/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
     String [][] datos = {
             {"Casco de Star Wars","100000","El casco de strom tropper para hijdf"},
-            {"Control","100000","El casco de strom tropper para hijdf"},
             {"Juguete de Sonic","100000","El casco de strom tropper para hijdf"}
     };
     int[] datosImg = {R.drawable.producto1,R.drawable.producto2,R.drawable.producto3};
