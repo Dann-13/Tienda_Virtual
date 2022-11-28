7da837f48e9cacb14684c36354ae9f435d167926
Dann-13 <dannyelgomezz.13@gmail.com>
2022-11-28 16:33:01 -0500

    Login y Registro con Firebase completado

--- app/src/main/java/com/example/tienda/login.java
+++ app/src/main/java/com/example/tienda/login.java
@@ -1,5 +1,6 @@
 package com.example.tienda;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,30 +9,56 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class login extends AppCompatActivity {
-    private EditText use, pas;
+    private EditText etCorreo, etContraseña;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        use = findViewById(R.id.use);
-        pas = findViewById(R.id.pas);
+        etCorreo = findViewById(R.id.editTextCorreo);
+        etContraseña = findViewById(R.id.editTextPassword);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+    public void ingresar(View view){
+        String correo = etCorreo.getText().toString();
+        String contraseña = etContraseña.getText().toString();
+
+        firebaseAuth.signInWithEmailAndPassword(correo,contraseña).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(login.this,"Ingreso Exitoso", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(login.this, ingreso.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(login.this, "Correo o Contraseña incorrectas", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void registrar(View view){
+        String correo = etCorreo.getText().toString();
+        String contraseña = etContraseña.getText().toString();
+        firebaseAuth.createUserWithEmailAndPassword(correo,contraseña).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(login.this, "Registro Exitoso", Toast.LENGTH_SHORT).show();;
+                }else{
+                    Toast.makeText(login.this,"Registro Mal", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        Toast.makeText(this, "Registro Exitoso", Toast.LENGTH_SHORT).show();
     }
 
-    public void validar (View view){
-
-        String usuario = use.getText().toString();
-        String clave = pas.getText().toString();
-
-        if (usuario.equals("admin") && clave.equals("12345")){
-            Intent i = new Intent(this, ingreso.class);
-            //i.putExtra("Nombre", usuario);
-            startActivity(i);
-        } else{
-            Toast mensaje = Toast.makeText(this, "Verifique usuario y/o contraseña", Toast.LENGTH_LONG);
-            mensaje.show();
-        }
-    }
 }
